819947d476f107bce3460920cba5820f97202570
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-10 15:03:48 +0700

    implement react-router 1 link

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import TodoApp from "./TodoApp";
 import styled from "styled-components";
 import { Button } from "reactstrap";
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./Home";
+
 const Container = styled.div`
   background-color: #e1e3de;
 `;
 
 class App extends Component {
   render() {
-    return <Container>
-        <h1>Learn React Form</h1>
-        <Button color="primary">Let's Go!</Button>
-        <TodoApp />
-      </Container>;
+    return (
+      <Router>
+        <div>
+          <Container>
+            <h1>Learn React Form</h1>
+            <Button color="primary">Let's Go!</Button>
+            <TodoApp />
+          </Container>
+          <Route path="/" exact component={Home} />
+          <Link to="/">Home</Link>
+        </div>
+      </Router>
+    );
   }
 }
 
