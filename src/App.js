c4c2c9e721696af0dfcc649434459b6fec205069
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-10 14:42:50 +0700

    try button reactstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import TodoApp from "./TodoApp";
 import styled from "styled-components";
+import { Button } from "reactstrap";
 
 const Container = styled.div`
   background-color: #e1e3de;
@@ -8,12 +9,11 @@ const Container = styled.div`
 
 class App extends Component {
   render() {
-    return (
-      <Container>
+    return <Container>
         <h1>Learn React Form</h1>
+        <Button color="primary">Let's Go!</Button>
         <TodoApp />
-      </Container>
-    );
+      </Container>;
   }
 }
 
