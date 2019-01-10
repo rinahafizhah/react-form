ee142aaeb9923f8111f7e6579fd81eeb0ef9104b
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-10 13:34:35 +0700

    add styling with styled-components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-
 import TodoApp from "./TodoApp";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #e1e3de;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <h1>Learn React Form</h1>
         <TodoApp />
-      </div>
+      </Container>
     );
   }
 }
--- src/TodoApp/index.js
+++ src/TodoApp/index.js
@@ -1,4 +1,52 @@
 import React, { Component } from "react";
+import styled from "styled-components"
+
+const Form = styled.form`
+display: flex;
+align-items: center;
+flex-direction: row;
+margin: 15px 0;
+`
+
+const Title = styled.h1`
+  color: rgb(0, 0, 255)
+`
+
+const Container = styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+`
+
+const Textarea = styled.input`
+border : 3px solid blue;
+border-radius: 10px;
+padding: 10px;
+width: 500px;
+`
+const Button = styled.input`
+  background-color: rgb(152, 212, 243);
+  border: 3px solid #6f7375;
+  border-radius: 10px;
+  padding: 10px;
+  box-shadow: 5px 5px;
+`;
+
+const Remove = styled.input`
+  background-color: rgb(152, 212, 243);
+  border: 3px;
+  border-radius: 10px;
+  padding: 10px;
+`;
+
+const Listtodo = styled.ul`
+  list-syle: none;
+  border: 1px solid #ccc;
+  margin-top: -1px;
+  line-height: 2;
+  padding: 2px 20px;
+  background: white;
+`;
 
 class TodoApp extends Component {
   constructor() {
@@ -48,7 +96,7 @@ class TodoApp extends Component {
       return (
         <li key={index}>
           {todo.id}:{todo.text}{" "}
-          <input
+          <Remove
             type="button"
             value="Remove"
             onClick={() => this.removeTodo(index)}
@@ -58,18 +106,18 @@ class TodoApp extends Component {
     });
 
     return (
-      <div>
-        <h1>Todo App</h1>
-        <form onSubmit={this.handleTodoSubmit}>
-          <input
+      <Container>
+        <Title>Todo App</Title>
+        <Form onSubmit={this.handleTodoSubmit}>
+          <Textarea
             type="text"
             placeholder="What's interesting today?"
             onChange={this.handleTodoInput}
           />
-          <input type="submit" value="Add Todo" />
-        </form>
-        <ul>{todoList}</ul>
-      </div>
+          <Button type="submit" value="Add Todo" />
+        </Form>
+        <Listtodo>{todoList}</Listtodo>
+      </Container>
     );
   }
 }
