2308f52e482720b49ac4416925b8662398878de0
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-11 11:16:34 +0700

    fix placing react-router   + fix pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import React, { Component } from "react";
-import TodoApp from "./TodoApp";
-import styled from "styled-components";
-import { Button } from "reactstrap";
+
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-const Container = styled.div`
-  background-color: #e1e3de;
-`;
+
 
 class App extends Component {
   render() {
-    return (
-      <Router>
+    return <Router>
         <div>
-          <Container>
-            <h1>Learn React Form</h1>
-            <Button color="primary">Let's Go!</Button>
-            <TodoApp />
-          </Container>
-          <Route path="/" exact component={Home} />
-          <Route path="/About" component={About} />
           <Link to="/">Home</Link>
           <Link to="/About">About</Link>
+          
+          <Route path="/" exact component={Home} />
+          <Route path="/About" component={About} />
         </div>
-      </Router>
-    );
+      </Router>;
   }
 }
 
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,8 +1,20 @@
 import React from "react";
+import TodoApp from "../TodoApp";
+import styled from "styled-components";
+import { Button } from "reactstrap";
+const Container = styled.div`
+  background-color: #e1e3de;
+`;
 
 class Home extends React.Component {
   render() {
-    return <h1>Welcome!</h1>;
+    return (
+      <Container>
+        <h1>Learn React Form</h1>
+        <Button color="primary">Let's Go!</Button>
+        <TodoApp />
+      </Container>
+    );
   }
 }
 
